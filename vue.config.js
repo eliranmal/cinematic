0bb95585321c06990a8fcb02d0dade30612b38a6
eliranmal <eliranmal@gmail.com>
2019-03-12 20:31:05 +0200

    chore(skeleton): vue init

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
+const pkg = require('./package.json');
+
+
+process.env.VUE_APP_NAME = pkg.name;
+process.env.VUE_APP_VERSION = pkg.version;
+
+
 module.exports = {
   lintOnSave: false,
+  publicPath: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: true,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined,
 };
