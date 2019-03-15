7ad568fed28105dd8728f7be455d42011734acd2
eliranmal <eliranmal@gmail.com>
2019-03-15 18:49:10 +0200

    feature(branding): baseline, typography
    - add baseline styles
    - add fonts

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img id="source-image" src="./assets/images/logo.png">-->
+    <HelloWorld msg="cinematic"/>
   </div>
 </template>
 
@@ -17,12 +17,13 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  #app {
+    font-family: 'harting', 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    margin-top: 60px;
+  }
+
 </style>
