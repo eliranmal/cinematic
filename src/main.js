fac4fafd9b21b60acfea650cda09fe82a280ff52
eliranmal <eliranmal@gmail.com>
2019-03-16 03:05:33 +0200

    feature(branding): baseline, typography
    show app info

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
+console.log(`< ${process.env.VUE_APP_NAME} v${process.env.VUE_APP_VERSION} >`);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app');
