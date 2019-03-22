e3cc2e213b23fb61873100fb20211af7bd7e4f99
eliranmal <eliranmal@gmail.com>
2019-03-22 23:43:08 +0200

    feature(components): gallery
    plugin v-show-slide

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './main.scss';
 import Vue from 'vue';
 import VueMq from 'vue-mq';
+import VueShowSlide from 'v-show-slide';
 import App from './App.vue';
 import { sortByValue } from './lib/object';
 import { breakpoints } from './config/layout';
@@ -11,6 +12,7 @@ Vue.use(VueMq, {
   // ensure lexical ordering of values, otherwise vue-mq breaks!
   breakpoints: sortByValue(breakpoints),
 });
+Vue.use(VueShowSlide);
 
 console.log(`< ${process.env.VUE_APP_NAME} v${process.env.VUE_APP_VERSION} >`);
 
